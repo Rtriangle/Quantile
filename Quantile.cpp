cc362c78c7c65c58c5b4eaf08285d5eae15a149f
Daniel Korbut <korbut.daniel@gmail.com>
2014-12-17 20:12:17 +0300

    Fast working

--- Quantile.cpp
+++ Quantile.cpp
@@ -4,6 +4,9 @@
 #include <cmath>
 #include <algorithm>
 #include <functional>
+#include <cmath>
+#include <math.h>
+#include <cstdio>
 #include <iterator>
 #pragma warning(disable : 4996)
 
@@ -11,25 +14,118 @@ template <typename Iterator, typename Comparator>
 void SiftUp(Iterator Begin, Iterator End, const Comparator &comparator)
 {
 	Iterator MoveElement = End;
-	while(comparator(*MoveElement, *(Begin + ((MoveElement - Begin - 1) / 2))))
-			iter_swap(MoveElement, Begin + ((MoveElement - Begin - 1) / 2));
+	while(Begin < MoveElement && comparator(*MoveElement, *(Begin + ((MoveElement - Begin - 1) / 2))))
+	{
+			std::iter_swap(MoveElement, Begin + ((MoveElement - Begin - 1) / 2));
+			MoveElement = Begin + ((MoveElement - Begin - 1) / 2);
+	}
 }
 
 template <typename Iterator, typename Comparator>
 void SiftDown(Iterator Begin, Iterator End, const Comparator &comparator)
 {
 	Iterator MoveElement = Begin;
-	while(true)
-	{
-		Iterator MoveTo = MoveElement;
-		if(comparator(*MoveElement, *(Begin + (Begin - MoveElement)*2+1)))
-			MoveTo = Begin + (Begin - MoveElement) * 2 + 1;
+	if((End - Begin) == 0)
+		return;
+	else
+		if(End - Begin == 1)
+		{
+			if(comparator(*MoveElement, *(Begin + (Begin - MoveElement)*2 + 1)))
+				std::iter_swap(Begin + (Begin - MoveElement)*2 + 1, MoveElement);
+		}
 		else
-			if(comparator(*MoveTo, *(Begin + (Begin - MoveElement) * 2 + 2)))
-				MoveTo = Begin + (Begin - MoveElement) * 2 + 2;
+		{
+			while(true)
+			{
+				Iterator MoveTo = MoveElement;
+				if(comparator(*MoveElement, *(Begin + (Begin - MoveElement)*2+1)))
+					MoveTo = Begin + (Begin - MoveElement) * 2 + 1;
+				else
+					if(comparator(*MoveTo, *(Begin + (Begin - MoveElement) * 2 + 2)))
+						MoveTo = Begin + (Begin - MoveElement) * 2 + 2;
+					else
+						return;
+				std::iter_swap(MoveTo, MoveElement);
+			}
+		}
+}
+
+//Все хорошо, только добавить для Heap1.empty() == true
+template <typename Container>
+void Solution(Container &Input, Container &Heap1, Container &Heap2,
+			  const size_t &N, size_t &Quantile, const double &alpha)
+{
+	std::cout << Input[0] << '\n';
+	size_t Tmp;
+	for(int i = 1; i < N; ++i)
+	{
+		Tmp = Quantile;
+		Quantile = std::floor(alpha * static_cast<double>(i));
+		if((Quantile - Tmp) == 0)
+		{
+			if(!Heap1.empty())
+			{
+				if(Input[i] <= *Heap1.begin())
+				{
+					Heap2.push_back(*Heap1.begin());
+					SiftUp(Heap2.begin(), Heap2.end()-1, std::less<int>());
+					Heap1[0] = Input[i];
+					SiftDown(Heap1.begin(), Heap1.end()-1, std::greater<int>());
+				}
+				else
+				{
+					Heap2.push_back(Input[i]);
+					SiftUp(Heap2.begin(), Heap2.end()-1, std::less<int>());
+				}
+			}
 			else
-				return;
-		std::iter_swap(MoveTo, MoveElement);
+			{
+				Heap2.push_back(Input[i]);
+				SiftUp(Heap2.begin(), Heap2.end()-1, std::less<int>());
+			}
+		}
+		else
+		{
+			if(!Heap1.empty())
+			{
+				if(Input[i] <= *Heap1.begin())
+				{
+					Heap1.push_back(Input[i]);
+					SiftUp(Heap1.begin(), Heap1.end()-1, std::greater<int>());
+				}
+				else
+				{
+					Heap1.push_back(Heap2[0]);
+					*Heap2.begin() = Input[i];
+					auto En = Heap2.end(); //!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+					En--;
+					auto Beg = Heap2.begin();//!!!!!!!!!!!!!!!!!!!!!!!
+					auto comp = std::less<int>();
+//					SiftDown(Heap2.begin(), Heap2.end()-1, std::less<int>());
+					SiftDown(Beg, En, comp);
+					SiftUp(Heap1.begin(), Heap1.end()-1, std::greater<int>());
+				}	
+			}
+			else
+			{
+				if(*Heap2.begin() < Input[i])
+				{
+					Heap1.push_back(Heap2[0]);
+					*Heap2.begin() = Input[i];
+					if(Heap2.size() != 0)
+					{
+						auto En = Heap2.end(); //!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+						En--;
+						auto Beg = Heap2.begin();//!!!!!!!!!!!!!!!!!!!!!!!
+						auto comp = std::less<int>();
+//						SiftDown(Heap2.begin(), Heap2.end()-1, std::less<int>());
+						SiftDown(Beg, En, comp);
+					}
+				}
+				else
+					Heap1.push_back(Input[i]);
+			}
+		}
 	}
 }
 
@@ -50,56 +146,6 @@ int main()
 		std::cin >> Tmp;
 		Input.push_back(Tmp);
 	}
-	Quantile = 0;
-	std::cout << Input[0] << '\n';
-	for(int i = 1; i < N; ++i)
-	{
-		Tmp = Quantile;
-		Quantile = std::floor(alpha * static_cast<double>(i));
-		if(Heap1.size() == 0 || Input[i] <= *(Heap1.begin()))
-			if((Quantile - Tmp) == 1)
-			{
-				Heap1.push_back(Input[i]);
-				SiftUp(Heap1.begin(),Heap1.end()-1, std::greater<int>());
-			}
-			else
-			{
-				if(!Heap1.empty())
-				{
-					size_t Tmp = Heap1[0];
-					Heap1[0] = Input[i];
-					Heap2.push_back(Tmp);
-					SiftDown(Heap1.begin(), Heap1.end()-1, std::greater<int>());
-				}
-				else
-					Heap2.push_back(Input[i]);
-				SiftUp(Heap2.begin(), Heap2.end()-1, std::less<int>());
-			}
-		else
-			if((Quantile - Tmp) == 1)
-			{
-				if(!Heap1.empty())
-				{
-					size_t Tmp = Heap2[0];
-					Heap1.push_back(*(Heap2.begin()));
-					Heap2[0] = Input[i];
-					SiftUp(Heap1.begin(), Heap1.end()-1, std::greater<int>());
-				}
-				else
-					Heap2.push_back(Input[i]);
-				SiftDown(Heap2.begin(), Heap2.end()-1, std::less<int>());
-			}
-			else
-			{
-				Heap2.push_back(Input[i]);
-				SiftUp(Heap2.begin(), Heap2.end()-1, std::less<int>());
-			}
-		if(!Heap1.empty())
-			std::cout << *Heap1.begin() << '\n';
-		else
-			std::cout << Input[0] << '\n';
-	}
-	fclose(stdin);
-	fclose(stdout);
+	Solution(Input, Heap1, Heap2, N, Quantile, alpha);
 	return 0;
 }
